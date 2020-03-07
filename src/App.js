f79b5b1c9d7a1f18161e05f98ae18fd2932f4fa9
Athinarayanan <athifrank@gmail.com>
2020-03-07 23:10:27 +0530

    spinner style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Calendar from './Components/Commons/Calendar'
 import Validation from './Components/Validation'
+import Spinner from './Components/Commons/Spinner'
 
 function App() {
 
   return (
     <div>
         {/* <Calendar /> */}
+         <Spinner shouldLoad={true}/>
         <Validation />
     </div>
   );
